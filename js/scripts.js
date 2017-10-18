45a9b6bd760c3dcd9e47086dffb19784cbbd55d8
Adam <alspdx@gmail.com>
2017-10-17 21:42:40 -0700

    add js function to remove li elements by clicking, add text to li element additions.

--- js/scripts.js
+++ js/scripts.js
@@ -1,6 +1,22 @@
 $(document).ready(function() {
   $("img#nicekitty").click(function() {
-    $("ul#kittymeow").prepend("<div>Dog, you are my slave and shall do my bidding.</div>");
-    $("ul#doggybark").prepend("<div>Bork, you are doing me a huge frighten.</div>");
+    $("ul#kittymeow").prepend("<li>Dog, you are my slave and shall do my bidding.</li>");
+    $("ul#doggybark").prepend("<li>Bork, you are doing me a huge frighten.</li>");
+    $("ul#kittymeow").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#doggybark").children("li").first().click(function() {
+      $(this).remove();
+    });
+  });
+  $("img#goodboy").click(function() {
+    $("ul#kittymeow").prepend("<li>What a fool you are!</li>");
+    $("ul#doggybark").prepend("<li>Can't we just play nice?</li>");
+    $("ul#kittymeow").children("li").first().click(function() {
+      $(this).remove();
+    });
+    $("ul#doggybark").children("li").first().click(function() {
+      $(this).remove();
+    });
   });
 });
