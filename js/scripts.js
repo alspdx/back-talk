bd0faee90a5c8e89828a9bf6270fb10a229be07e
Adam <alspdx@gmail.com>
2017-10-17 21:24:16 -0700

    add js function to click image and add li elements

--- js/scripts.js
+++ js/scripts.js
@@ -1,3 +1,6 @@
-$("img#nicekitty").click(function() {
-  
-})
+$(document).ready(function() {
+  $("img#nicekitty").click(function() {
+    $("ul#kittymeow").prepend("<div>Dog, you are my slave and shall do my bidding.</div>");
+    $("ul#doggybark").prepend("<div>Bork, you are doing me a huge frighten.</div>");
+  });
+});
