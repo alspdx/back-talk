23b812b13300286d4b9f5e4d9384d769dec60949
Adam <alspdx@gmail.com>
2017-10-17 23:15:38 -0700

    add button to switch between two page styles

--- js/scripts.js
+++ js/scripts.js
@@ -1,4 +1,5 @@
 $(document).ready(function() {
+// Cat and Dog Fight
   $("img#nicekitty").click(function() {
     $("ul#kittymeow").prepend("<li>Dog, you are my slave and shall do my bidding.</li>");
     $("ul#doggybark").prepend("<li>Bork, you are doing me a huge frighten.</li>");
@@ -18,5 +19,18 @@ $(document).ready(function() {
     $("ul#doggybark").children("li").first().click(function() {
       $(this).remove();
     });
+  });
+// Button Toggle
+  $("div.buttontoggler").click(function() {
+    $("button.buttonhidden").toggle();
+    $("button.buttonshowing").toggle();
+  })
+  $("button#buttondarktheme").click(function() {
+    $("body").removeClass();
+    $("body").addClass("darktheme");
+  });
+  $("button#buttoncolortheme").click(function() {
+    $("body").removeClass();
+    $("body").addClass("colortheme");
   });
 });
